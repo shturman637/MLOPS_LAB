6788c10b4566381a149ac6c0bfc73bca1f229167
Sergey Sokolov <152925976+shturman637@users.noreply.github.com>
2024-06-23 23:11:19 +0300

    Update model_preparation.py

--- lab1/model_preparation.py
+++ lab1/model_preparation.py
@@ -1,14 +1,48 @@
-import os
-import pickle
-import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
-base_dir = ''  # Укажите базовую директорию, где находятся ваши данные
-# Загрузка предобработанных данных
-X_train = pd.read_csv(os.path.join(base_dir, 'train', 'data_train_preprocessed.csv'))
-y_train = pd.read_csv(os.path.join(base_dir, 'train', 'temp_train_preprocessed.csv'))
-# Обучение модели
-model = RandomForestRegressor()
-model.fit(X_train, y_train)
-# Сохранение модели
-with open(os.path.join(base_dir, 'model.pkl'), 'wb') as f:
-    pickle.dump(model, f)
+import pandas as pd                                 # Библиотека Pandas для работы с табличными данными
+from sklearn.preprocessing import StandardScaler    # Импортируем стандартизацию от scikit-learn
+from sklearn.pipeline import Pipeline               # Пайплайн
+
+
+# Загрузим данные из файла "data_train.csv"
+DF_train = pd.read_csv('train/data_train.csv')
+
+# Выполним предобработку данных с помощью sklearn.preprocessing.StandardScaler
+scaler = StandardScaler()
+
+# Обучаем на тренировочных данных
+X_train = DF_train['x']
+
+# Преобразование Series в DataFrame с одним столбцом
+data_frame = X_train.to_frame()
+
+# Стандартизируем данные
+X_train_prep = scaler.fit_transform(data_frame)
+
+# Запишем данные в файл
+DF_train['x'] = X_train_prep
+#print(DF_train)
+DF_train.to_csv(f'train/data_train_prep.csv', index=False)
+print("Стандартизованные тренировочные данные записаны в файл data_train_prep.csv")
+
+# Загрузим данные из файла "data_test.csv"
+DF_test = pd.read_csv('test/data_test.csv')
+
+# Обучаем на тренировочных данных
+X_test = DF_test['x']
+
+#print(X_train)
+# Выполним предобработку данных с помощью sklearn.preprocessing.StandardScaler
+scaler = StandardScaler()
+
+# Преобразование Series в DataFrame с одним столбцом
+data_frame = X_test.to_frame()
+
+# Стандартизируем данные
+X_test_prep = scaler.fit_transform(data_frame)
+#print(X_train_prep)
+
+# Запишем данные в файл
+DF_test['x'] = X_test_prep
+print(DF_test)
+DF_test.to_csv(f'test/data_test_prep.csv', index=False)
+print("Стандартизованные тестовые данные записаны в файл data_train_prep.csv")
