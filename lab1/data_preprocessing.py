33f409777922909352b4c390fd4e7d723581407c
shturman <shturman2605@mail.ru>
2024-06-23 06:27:06 +0300

    update data_preprocessing.py

--- lab1/data_preprocessing.py
+++ lab1/data_preprocessing.py
@@ -1,15 +1,24 @@
-import osimport pandas as pd
+import os
+import pandas as pd
 from sklearn.preprocessing import StandardScaler
-# Укажите базовую директорию, где находятся ваши данныеbase_dir = ''
+# Укажите базовую директорию, где находятся ваши данные
+base_dir = ''
 # Загрузка данных
-train_data = pd.read_csv(os.path.join(base_dir, 'train', 'data_train.csv'))test_data = pd.read_csv(os.path.join(base_dir, 'test', 'data_test.csv'))
+train_data = pd.read_csv(os.path.join(base_dir, 'train', 'data_train.csv'))
+test_data = pd.read_csv(os.path.join(base_dir, 'test', 'data_test.csv'))
 # Определяем признаки и целевую переменную
-X_train = train_data.drop(columns=['temperature'])y_train = train_data['temperature']
-X_test = test_data.drop(columns=['temperature'])y_test = test_data['temperature']
+X_train = train_data.drop(columns=['temperature'])
+y_train = train_data['temperature']
+X_test = test_data.drop(columns=['temperature'])
+y_test = test_data['temperature']
 # Отделяем числовые признаки для стандартизации
 numerical_features = X_train.select_dtypes(include=['float64', 'int64']).columns
-# Стандартизируем числовые признакиscaler = StandardScaler()
-X_train[numerical_features] = scaler.fit_transform(X_train[numerical_features])X_test[numerical_features] = scaler.transform(X_test[numerical_features])
+# Стандартизируем числовые признаки
+scaler = StandardScaler()
+X_train[numerical_features] = scaler.fit_transform(X_train[numerical_features])
+X_test[numerical_features] = scaler.transform(X_test[numerical_features])
 # Сохранение предобработанных данных
-X_train.to_csv(os.path.join(base_dir, 'train', 'data_train_preprocessed.csv'), index=False)X_test.to_csv(os.path.join(base_dir, 'test', 'data_test_preprocessed.csv'), index=False)
-y_train.to_csv(os.path.join(base_dir, 'train', 'temp_train_preprocessed.csv'), index=False)y_test.to_csv(os.path.join(base_dir, 'test', 'temp_test_preprocessed.csv'), index=False)
+X_train.to_csv(os.path.join(base_dir, 'train', 'data_train_preprocessed.csv'), index=False)
+X_test.to_csv(os.path.join(base_dir, 'test', 'data_test_preprocessed.csv'), index=False)
+y_train.to_csv(os.path.join(base_dir, 'train', 'temp_train_preprocessed.csv'), index=False)
+y_test.to_csv(os.path.join(base_dir, 'test', 'temp_test_preprocessed.csv'), index=False)
