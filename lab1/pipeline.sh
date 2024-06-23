d7cf7927952ca6d630f7594a5f27a1ab54c65a0a
shturman <shturman2605@mail.ru>
2024-06-23 06:28:06 +0300

    update pipeline.sh

--- lab1/pipeline.sh
+++ lab1/pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 python3 ./MLOPS_LAB/lab1/data_creation.py 
-python3 ./MLOPS_LAB/lab1/model_preprocessing.py 
+python3 ./MLOPS_LAB/lab1/data_preprocessing.py 
 python3 ./MLOPS_LAB/lab1/model_preparation.py 
 python3 ./MLOPS_LAB/lab1/model_testing.py 
  
